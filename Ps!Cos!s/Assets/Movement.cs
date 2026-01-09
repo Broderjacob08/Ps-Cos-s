87376a4a4d1d43795c76ef070d4ceaa1221e519e
Broderjacob08 <alexander.hammarlund@elev.ga.lbs.se>
2026-01-09 12:15:05 +0100

    Started doing movemnt

---
+++ Ps!Cos!s/Assets/Movement.cs
@@ -0,0 +1,46 @@
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 0.5f;
+    Rigidbody2D rb;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+
+        rb = GetComponent<Rigidbody2D>();
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        rb.linearVelocity = new Vector2(0,0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.linearVelocity = new Vector2(0, 1) * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.linearVelocity = new Vector2(0, -1) * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.linearVelocity = new Vector2(1, 0) * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.linearVelocity = new Vector2(-1, 0) * speed;
+        }
+        if(Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W))
+        {
+            rb.linearVelocity = new Vector2(-1, 1) * speed;
+        }
+    }
+}
