221c5f501ddd1baacaeaf43c938af6db94916ac7
i-j-p-g <ian.jimenezpradosberg@elev.ga.lbs.se>
2026-01-16 13:02:46 +0100

    fixed item bug

---
+++ Ps!Cos!s/Assets/Scripts/Items/Item.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum ItemType
+    {
+        key,
+        keycard
+    }
+
+    public ItemType itemType;
+
+    public string itemName;
+
+
+}
