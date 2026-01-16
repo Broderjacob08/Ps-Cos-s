6feb1302a022fa3b104eddb4402b35153dd51339
i-j-p-g <ian.jimenezpradosberg@elev.ga.lbs.se>
2026-01-16 15:09:09 +0100

    fixed stuff

--- Ps!Cos!s/Assets/Scripts/GrowlingFleshHound.cs
+++ Ps!Cos!s/Assets/Scripts/GrowlingFleshHound.cs
@@ -1,20 +1,24 @@
+using Unity.VisualScripting;
+using UnityEditor.Timeline;
 using UnityEngine;
 
 public class FleshHoundNoises : MonoBehaviour
 {
+    private GameObject player;
     public AudioSource Walking;
     public AudioSource Growling;
     int time = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        player = FindObjectOfType<Movement>().gameObject;
         if(Walking.isPlaying == false)
         {
             Walking.Play();
         }
         
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    /*private void OnTriggerEnter2D(Collider2D collision)
     {
         Walking.enabled = true;
         Growling.enabled = true;
@@ -23,17 +27,35 @@ public class FleshHoundNoises : MonoBehaviour
     {
         Walking.enabled = false;
         Growling.enabled = false;
-    }
+    }*/
 
     // Update is called once per frame
     void Update()
     {
-        if (Growling.isPlaying == false && time == 1000)
+        time += 1;
+
+        
+
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+        if (distance <= 13)
         {
-            Growling.Play();
+            Walking.enabled = true;
+            Growling.enabled = true;
+            if(Walking.isPlaying == false)
+            {
+                Walking.Play();
+            }
+            
+            if (Growling.isPlaying == false && time >= 1000)
+            {
+                Growling.Play();
+                time = 0;
+            }
         }
-
-
-            time += 1;
+        else
+        {
+            Walking.enabled = false;
+            Growling.enabled = false;
+        }
     }
 }
